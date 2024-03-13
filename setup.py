495109b9227b4ce85d9c2510dfec34f8575b95e8
Pushkar-Morankar <thepushkarmorankargmail.com>
2024-03-14 00:08:03 +0530

    File updated

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-     
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgeneraor',
+    version='0.0.1',
+    author='Pushkar-Morankar',
+    author_email='thepushkarmorankar@gmail.com',
+    install_requires=["openai","langchian","streamlit","python-dotenv","PyPDF2"],
+    packages= find_packages()
+)
