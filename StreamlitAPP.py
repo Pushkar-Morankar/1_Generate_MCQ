bee169623949dc5ad10c15261440bce0b477f628
Pushkar-Morankar <thepushkarmorankargmail.com>
2024-03-20 20:22:40 +0530

    new

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -5,7 +5,5 @@ import pandas as pd
 from dotenv import load_dotenv
 from src.mcq_generator.utils import read_file,get_table_data
 import streamlit as st
-#from langchain_community.chat_models import ChatOpenAI
-#from langchain_community.callbacks import get_openai_callback
 from src.mcq_generator.MCQ_generator import generate_evaluate_chain
 from src.mcq_generator.logger import logging
